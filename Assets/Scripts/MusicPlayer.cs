d6fac1551751c8dd71566af9ccd34b7252a682f2
Maxime Buttard <maxime.buttard@gmail.com>
2017-07-26 16:40:19 +0200

    ajout musique dans le jeu

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour {
 	static MusicPlayer instance = null;
 	
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip endClip;
+	
+	private AudioSource music;
+	
 	void Start () {
 		if (instance != null && instance != this) {
 			Destroy (gameObject);
@@ -11,7 +17,32 @@ public class MusicPlayer : MonoBehaviour {
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
-		}
+			music = GetComponent<AudioSource>();
+			music.clip = startClip;
+			music.loop = true;
+			music.Play();
+		}		
+	}
+	
+	void OnLevelWasLoaded(int level) {
+		Debug.Log("MusicPlayer Loaded level " + level);
+		music.Stop();
 		
+		switch(level) {
+			case 0:
+				music.clip = startClip;
+				break;
+			case 1:
+				music.clip = gameClip;
+				break;
+			case 2:
+				music.clip = endClip;
+				break;
+			default:
+				music.clip = startClip;
+				break;
+		}
+		music.loop = true;
+		music.Play();
 	}
 }
